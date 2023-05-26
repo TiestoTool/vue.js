7a9b718008a8c40effe424a5f862c52ce1b0d9fb
TiestoTool <112871170+TiestoTool@users.noreply.github.com>
2023-05-26 09:48:56 +0200

    commit

--- weatherapp/src/components/BaseModal.vue
+++ weatherapp/src/components/BaseModal.vue
@@ -1,10 +1,32 @@
 <template>
-    <div class="absolute w-full bg-black">
+<Transition name="modal-outer">
+    <div
+    v-show="modalActive"
+     class="absolute w-full bg-black bg-opacity-30 h-screen
+    top-0 left-0 felx justify-center px-8"
+    >
+<Transition name="modal-inner">
+    <div v-if="modalActive"
+    class="p-4 bg-white self-start mt-32 max-w-screen-md">
+        <slot />
+        <button class=" text-white mt-8 bg-weather-primary py-2 px-6"
+        @click="$emit('close-modal')">
+            close
+        </button>
     </div>
+</Transition>
+    </div>
+</Transition>
 </template>
 
 <script setup>
-
+defineEmits(["close-modal"]);
+defineProps({
+    modalActive: {
+        type:Boolean,
+        default:false
+    }
+})
 </script>
 
 <style lang="scss" scoped>
--- weatherapp/src/components/SiteNavigation.vue
+++ weatherapp/src/components/SiteNavigation.vue
@@ -2,7 +2,7 @@
     <header class="sticky top-0 bg-weather-primary shadow-lg">
         <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
 
-            <RouterLink :to ="{ name: home}">
+            <RouterLink :to="{ name: home }">
                 <div class="flex items-center gap-3">
                     <i class="fa-solid fa-sun" text-2xl></i>
                     <p class="text-2xl ">Local weather</p>
@@ -12,18 +12,35 @@
             <div class=" flex gap-3 flex-1 justify-end">
 
                 <i class="fa-solid fa-circle-info text-xl
-                hover:text-weather-secondary duration-150 cursor-pointer">
+                hover:text-weather-secondary duration-150 cursor-pointer" @click="toggleModal">
                 </i>
                 <i class="fa-solid fa-plus text-xl
                 hover:text-weather-secondary duration-150 cursor-pointer">
                 </i>
 
             </div>
+            <BaseModal :modalActive="modalActive" @close-modal="toggleModal">
+                <div class="text-black">
+                    <h1>
+                        About:
+                    </h1>
+                    <p>
+                        This app tracks the local weather. This is made by Tiësto in Veu.js and tailwindCSS
+                    </p>
+                </div>
+            </BaseModal>
         </nav>
     </header>
 </template>
 
 <script setup>
+import {ref} from "vue";
 import { RouterLink } from 'vue-router';
+import BaseModal from './BaseModal.vue';
+
+const modalActive = ref(null);
+const toggleModal = () => {
+    modalActive.value = !modalActive.value;
+}
 </script>
 
